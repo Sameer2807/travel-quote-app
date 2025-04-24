2df6a2a6333892cb8684500b35e47a8fb6825fad
Sameer2807 <113500901+Sameer2807@users.noreply.github.com>
2025-04-25 00:14:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from trip_info_form import show_trip_info_form
 from itinerary_planner import show_itinerary_planner
 
-# Initialize the session variable if not already
+# Session-based navigation
 if "current_page" not in st.session_state:
     st.session_state.current_page = "form"
 
-# Routing based on session state
 if st.session_state.current_page == "form":
     show_trip_info_form()
 elif st.session_state.current_page == "itinerary":
